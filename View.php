722199d88ad499cce56553087c0913cb6f59d6de
Ariel Toure <arielz360007@gmail.com>
2025-12-23 13:31:54 +0000

    Update View.php

--- View.php
+++ View.php
@@ -2,10 +2,16 @@
 session_start();
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View</title>
 
 <?php
 //Base de Donées stockées dans l'app Mysql
-$Dbname="Db_1";
+$dbname="Db_1";
 $host="127.0.0.1";
 $port=3307;
 $user="webuser";
@@ -13,7 +19,7 @@ $password="motdepassefort";
 
 
 try{
-    $connect = new PDO("mysql:host=$host;dbname=$Dbname;port=$port", $user, $password);
+    $connect = new PDO("mysql:host=$host;dbname=$dbname;port=$port", $user, $password);
     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 }catch(PDOException $e){
@@ -24,7 +30,7 @@ try{
 
 <?php
   //Recuperation des données
-  $stmt=$connect->prepare('SELECT id, email FROM tri');
+  $stmt=$connect->prepare('SELECT id, email, nom FROM tri');
   $stmt->execute();
   $data=$stmt->fetch(FETCH_ASSOC);
   ?>
@@ -32,7 +38,22 @@ try{
 <?php
 if(!empty($data)){
   foreach($data as $row): ?>
+      <h1>Données d'utilisateurs</h1>
 
+     <?php echo htmlspecialchars($row['id']); ?>
+      <p>Nom: <?php echo htmlspecialchars($row['nom']); ?></p>
 
+     <?php echo htmlspecialchars($row['id']); ?>
+      <p>Email: <?php echo htmlspecialchars($row['email']); ?></p>
+    <?php }else{
+    echo "Aucun utilisateur.";
+    }?>
+<?php endforeach; ?>
+<?php endif; ?>
+
+    </head>
+<body>
     
+</body>
+</html>
 
